45c9731be78f2469763df3573147dca8d9a01836
Alejo Gabriel <acunaalejo05@gmail.com>
2026-06-09 11:11:23 -0300

    agregamos la estructura de practica con js

---
+++ js/01-sumatoria.js
@@ -0,0 +1,12 @@
+// Realizar una suma de dos numeros y mostrar el resultado
+
+// Crear variables
+
+const numero1 = parseInt(prompt("Ingresa el primer número"))
+console.log(numero1)
+
+const numero2 = parseInt(prompt("Ingresa el segundo numero"))
+console.log(numero2)
+
+const resultado = numero1 + numero2
+console.log("La suma de los dos numeros es: " + resultado)
